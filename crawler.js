eb291b427fb9dff0d4ce19b1cd3bfdf6d9150255
constantine111 <constantine111@outlook.com>
2018-07-17 18:01:47 +0100

    changes made so the crawler goes through every sub page.

--- crawler.js
+++ crawler.js
@@ -6,33 +6,68 @@
 */
 var request = require('request');
 var cheerio = require('cheerio');
-var URL = require('url-parser');
-var fs = require('fs');
-var csv = require('fast-csv');
-var ws = fs.createWriteStream('crawlerData.csv');
+var URL = require('url-parse');
 
-/* Provided URL to crawl */
-var pageToVisit = "https://www.caseblocks.com/";
+var START_URL = "https://caseblocks.com/";
+var pageToVisit = START_URL;
+var pagesVisited = {};
+var numPagesVisited = 0;
+var pagesToVisit = [];
+var url = new URL(START_URL);
+var baseUrl = url.protocol + "//" + url.hostname;
 
-  request(pageToVisit, function(error, response, body) {
-    if(error) {
-      console.log("Error: " + error);
+request(pageToVisit, function(error, response, body) {
+  var serverStatus = response.statusCode; // Check status code.
+  var $ = cheerio.load(body); //select the website body.
+
+  pagesToVisit.push(START_URL);
+  setInterval(crawl,5000);
+
+  function crawl() {
+    var nextPage = pagesToVisit.pop();
+    if (!nextPage) {
+        return;
+    } else if (nextPage in pagesVisited) {
+      crawl();
+    } else {
+      visitPage(nextPage, crawl);
     }
 
-    var serverStatus = response.statusCode; // Check status code.
-    var $ = cheerio.load(body); //select the website body.
-    var pageTitle = ($('title').text()); //select the page title.
-    if(response.statusCode === 200) {
-      // Parse the document title.
-      console.log("Page title:  " + $('title').text());
-    }
-      // Writing to the CSV File.
-      // csv comes from the 'fast-csv' module.
-    csv
-      .write([
-        ["Page Visiting:" + pageToVisit],
-        ["Response Status:" + serverStatus],
-        ["Page Title:" + pageTitle]
-      ],{headers:true})
-      .pipe(ws);
+  }
+
+  function visitPage(url, callback, body) {
+    // Add page to our set
+    pagesVisited[url] = true;
+    numPagesVisited++;
+
+    // Make the request
+    console.log("Visiting page " + url);
+    request(url, function(error, response, body) {
+       // Check status code (200 is HTTP OK)
+       if(response.statusCode !== 200) {
+         callback();
+         return;
+       }else{
+         console.log(error);
+       }
+       // Parse the document body
+       var $ = cheerio.load(body);
+         collectInternalLinks($);
+        
+         callback();
+
     });
+  }
+
+
+  function collectInternalLinks($) {
+      var pageTitle = ($('title').text());
+      console.log(pageTitle);
+      console.log(response.statusCode);
+      var relativeLinks = $("a[href^='/']");
+      relativeLinks.each(function() {
+          pagesToVisit.push(baseUrl + $(this).attr('href'));
+      });
+  }
+
+});
